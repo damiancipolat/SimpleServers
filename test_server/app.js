5d76644f5c6fa8d448a793ba58c4175d0d914ad4
DamCipolat <damian.cipolat@gmail.com>
2019-06-19 17:28:37 -0300

    Update app.js

--- test_server/app.js
+++ test_server/app.js
@@ -39,7 +39,7 @@ app.get('/test',(req,res)=>{
 	const response = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 	
   	//Envio la respuesta.
-  	res.json({"status":"ok","body:response});
+  	res.json({"status":"ok","body":response});
 });
 
 //Agrego esta ruta, para los casos en que se escribe mal la url para obtener siempre un retorno.
